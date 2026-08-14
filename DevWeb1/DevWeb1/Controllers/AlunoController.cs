1d18ffc53bb21e3ca56f52f70221f24e0bb3adf5
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2026-08-14 15:19:34 -0300

    Dev Web

---
+++ DevWeb1/DevWeb1/Controllers/AlunoController.cs
@@ -0,0 +1,32 @@
+﻿using DevWeb1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevWeb1.Controllers
+{
+    [ApiController]
+    [Route("Aluno")]
+    public class AlunoController : ControllerBase
+    {
+        //sintaxe de uma função é a visibilidade da função
+        // visibilidade da função: public, private, protected
+        //pritvate so a classe acessar
+        //Tipo de retorno
+        //nome da funça
+        //parâmetros
+        [HttpGet]
+
+        public IActionResult Saudacao()
+        {
+            return Ok("Oi H1");
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Aluno aluno)
+        {
+            //imagine que vou no gravar banco de dados
+            return Ok("Cadastrado com sucesso");
+        }
+        
+    }
+
+}
---
+++ DevWeb1/DevWeb1/Models/Aluno.cs
@@ -0,0 +1,11 @@
+﻿namespace DevWeb1.Models
+{
+    //classe anemica
+    public class Aluno
+    {
+        public string Nome { get; set; } //prop tab tab cria a propriedade
+        public string Email { get; set; }
+        public string RA { get; set; }     
+
+    }
+}
